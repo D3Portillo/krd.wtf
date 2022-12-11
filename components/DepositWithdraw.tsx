5731a919cdc2c2d584a8b11603f3141db692da3f
D3Portillo <d3portillo@gmail.com>
2022-12-11 18:35:46 +0100

    feat(DepositWithdraw.tsx): formatCurrency for user balance

--- components/DepositWithdraw.tsx
+++ components/DepositWithdraw.tsx
@@ -1,10 +1,12 @@
-function DepositWithdraw() {
+import { formatCurrency } from "@/lib/currency"
+
+function DepositWithdraw({ balance = 0 }: { balance?: number | string }) {
   return (
     <div className="w-full z-10 fixed bottom-0 left-0">
       <div className="bg-gradient-to-b from-transparent to-white h-12 -mb-1" />
       <section className="bg-white w-full max-w-screen-sm mx-auto drop-shadow text-center rounded-t-3xl p-4 z-10">
         <h2 className="text-zinc-400 text-sm">Card Balance</h2>
-        <strong className="text-2xl">123.32 EUR</strong>
+        <strong className="text-2xl">{formatCurrency(balance)} USD</strong>
         <nav className="flex items-center gap-4 mt-8 font-bold">
           <button className="border rounded-full py-4 px-8 w-full">
             TOP UP
